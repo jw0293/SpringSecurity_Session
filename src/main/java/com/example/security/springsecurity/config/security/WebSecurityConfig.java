2c7c4f9b258368869d3bdc255134f169319b87cf
jw0293 <k38836919@gmail.com>
2022-07-14 13:49:10 +0900

    Refactor Project

---
+++ src/main/java/com/example/security/springsecurity/config/security/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package com.example.security.springsecurity.config.security;
+
+import com.example.security.springsecurity.handler.CustomLoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                // /about 요청에 대해서는 로그인 요구
+                .antMatchers("/about").authenticated()
+                // /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
+                .antMatchers("/admin").hasRole("ADMIN")
+                // 나머지 요청에 대해서는 로그인을 요구하지 않음
+                .anyRequest().permitAll()
+                .and()
+                // 로그인하는 경우에 대해 설정
+                .formLogin()
+                // 로그인 페이지 제공하는 URL 설정
+                .loginPage("/user/loginView")
+                // 로그인 성공 URL 설정
+                .successForwardUrl("/index")
+                // 로그인 실패 URL 설정
+                .failureForwardUrl("/index")
+                .permitAll()
+                .and()
+                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception{
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/user/login");
+        customAuthenticationFilter.setAuthenticationSuccessHandler(customLoginSuccessHandler());
+        customAuthenticationFilter.afterPropertiesSet();
+        return customAuthenticationFilter;
+    }
+
+    @Bean
+    public CustomLoginSuccessHandler customLoginSuccessHandler(){
+        return new CustomLoginSuccessHandler();
+    }
+
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider(){
+        return new CustomAuthenticationProvider(userDetailsService, bCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider());
+    }
+}
